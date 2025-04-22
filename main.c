d63204b85cfcb234f08fa6c3dfe2f2dffa237a29
baal <alburquenque.letelier@gmail.com>
2025-04-22 10:38:34 -0400

    Se imprime informacion del parseo

--- main.c
+++ main.c
@@ -11,10 +11,9 @@ int main(int argc, char *argv []) {
     int cities_size = 0;
     csv_processor(argv[1], &cities, &cities_size);
 
-
-
-
-    
+    for(int i = 0; i<cities_size; i++) {
+        printf("City: %s, Siesmic_Level: %d, Risk_Percent: %.2f\n", cities[i].city_name, cities[i].seismic_level, cities[i].risk_percent);
+    }
 
 
 
