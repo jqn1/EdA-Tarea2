9eb927685d30555af7b3f599fbf4365446da7b93
jqn1 <154387579+jqn1@users.noreply.github.com>
2025-04-22 18:45:06 -0400

    Añadir csv de prueba grande

--- main.c
+++ main.c
@@ -15,14 +15,14 @@ int main(int argc, char *argv []) {
     
 
      
-    for(int i = 0; i<cities_size; i++) {
+    for(int i = 0; i < cities_size; i++) {
         printf("City: %s, Siesmic_Level: %d, Risk_Percent: %.2f\n", cities[i].city_name, cities[i].seismic_level, cities[i].risk_percent);
     }
 
     //primer sort
     quickSort(cities, 0, cities_size - 1, get_seismic_level);
     printf("\n Primer Sort: \n");
-    for(int i = 0; i<cities_size; i++) {
+    for(int i = 0; i < cities_size; i++) {
         printf("City: %s, Siesmic_Level: %d, Risk_Percent: %.2f\n", cities[i].city_name, cities[i].seismic_level, cities[i].risk_percent);
     }
 
