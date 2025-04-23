9e59ac814baa6cf87760c7b59862c22fbcbb6513
baal <alburquenque.letelier@gmail.com>
2025-04-22 22:18:51 -0400

    Add quicksort by risk_percent
    Se aplica quicksort en base a los 5 niveles sismicos sobre el puntero al arreglo cities. \n Pendiente: Aplicar regla de ris_percent según nivel sismico.

--- main.c
+++ main.c
@@ -3,67 +3,65 @@
 #include "modules/processor.h"
 #include "modules/sorter.h"
 
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char *argv []) {
-    
-    if (argc != 4) {
-        printf("Uso: %s <input> <N> <output>\n", argv[ 0 ]);
+    if (argc != 4)
+    {
+        printf("Uso: %s <input> <N> <output>\n", argv[0]);
         return 1;
     }
 
     char *input_name = argv[1];
     int cities_to_priorize = atoi(argv[2]);
     char *output_name = argv[3];
-    
 
     City *cities = NULL;
     int cities_size = 0;
-    csv_processor(input_name, &cities, &cities_size); 
-
-
-    
+    csv_processor(input_name, &cities, &cities_size);
+    printf("Cities size: %d", cities_size);
 
     // Lista original
     printf("Lista original:\n");
-    for(int i = 0; i < cities_size; i++) {
-        printf("City: %s, Siesmic_Level: %d, Risk_Percent: %.2f\n", cities[i].city_name, cities[i].seismic_level, cities[i].risk_percent);
-    }
-
-    //primer sort
+    // for(int i = 0; i < cities_size; i++) {
+    //     printf("City: %s, Siesmic_Level: %d, Risk_Percent: %.2f\n", cities[i].city_name, cities[i].seismic_level, cities[i].risk_percent);
+    // }
+    
+    // primer sort
     quickSort(cities, 0, cities_size - 1, get_seismic_level);
     printf("\n Primer Sort: \n");
-    printf("%f", 0.0f/0.0f);
     // for(int i = 0; i < cities_size; i++) {
     //     printf("City: %s, Siesmic_Level: %d, Risk_Percent: %.2f\n", cities[i].city_name, cities[i].seismic_level, cities[i].risk_percent);
     // }
 
-    //segundo sort según risk_percent;
-    int l1_seismic_count = 0; 
-    int l2_seismic_count = 0; 
-    int l3_seismic_count = 0; 
-    int l4_seismic_count = 0; 
-    int l5_seismic_count = 0; 
-    for(int i=0; i < cities_size; i++){
+    // segundo sort según risk_percent;
+    int l1_seismic_count = 0;
+    int l2_seismic_count = 0;
+    int l3_seismic_count = 0;
+    int l4_seismic_count = 0;
+    int l5_seismic_count = 0;
+    for (int i = 0; i < cities_size; i++)
+    {
         l1_seismic_count = cities[i].seismic_level == 1 ? l1_seismic_count + 1 : l1_seismic_count;
         l2_seismic_count = cities[i].seismic_level == 2 ? l2_seismic_count + 1 : l2_seismic_count;
         l3_seismic_count = cities[i].seismic_level == 3 ? l3_seismic_count + 1 : l3_seismic_count;
-        l4_seismic_count = cities[i].seismic_level == 4 ? l4_seismic_count + 1 : l5_seismic_count;
+        l4_seismic_count = cities[i].seismic_level == 4 ? l4_seismic_count + 1 : l4_seismic_count;
         l5_seismic_count = cities[i].seismic_level == 5 ? l5_seismic_count + 1 : l5_seismic_count;
     }
-    // printf("l1_count: %d, l2_count: %d, l3_count: %d, l4_count: %d, l5_count: %d \n", l1_seismic_count, l2_seismic_count, l3_seismic_count, l4_seismic_count, l5_seismic_count);
+    printf("l1_count: %d, l2_count: %d, l3_count: %d, l4_count: %d, l5_count: %d \n", l1_seismic_count, l2_seismic_count, l3_seismic_count, l4_seismic_count, l5_seismic_count);
+    printf("total: %d", l1_seismic_count + l2_seismic_count + l3_seismic_count + l4_seismic_count + l5_seismic_count);
     // se hace un quicksort por cada nivel sismico
-    quickSort(cities, 0, l1_seismic_count-1, get_risk_percent);
-    quickSort(cities, l1_seismic_count, l1_seismic_count+l2_seismic_count-1, get_risk_percent);
-    quickSort(cities, l1_seismic_count+l2_seismic_count, l1_seismic_count+l2_seismic_count+l3_seismic_count-1, get_risk_percent);
-    quickSort(cities, l1_seismic_count+l2_seismic_count+l3_seismic_count, l1_seismic_count+l2_seismic_count+l3_seismic_count+l4_seismic_count-1, get_risk_percent);
-    quickSort(cities, l1_seismic_count+l2_seismic_count+l3_seismic_count+l4_seismic_count, l1_seismic_count+l2_seismic_count+l3_seismic_count+l4_seismic_count+l5_seismic_count-1, get_risk_percent);
-    quickSort(cities, l1_seismic_count+l2_seismic_count+l3_seismic_count+l4_seismic_count+l5_seismic_count, cities_size-1, get_risk_percent);
+    quickSort(cities, 0, l1_seismic_count - 1, get_risk_percent);
+    quickSort(cities, l1_seismic_count, l1_seismic_count + l2_seismic_count - 1, get_risk_percent);
+    quickSort(cities, l1_seismic_count + l2_seismic_count, l1_seismic_count + l2_seismic_count + l3_seismic_count - 1, get_risk_percent);
+    quickSort(cities, l1_seismic_count + l2_seismic_count + l3_seismic_count, l1_seismic_count + l2_seismic_count + l3_seismic_count + l4_seismic_count - 1, get_risk_percent);
+    quickSort(cities, l1_seismic_count + l2_seismic_count + l3_seismic_count + l4_seismic_count, l1_seismic_count + l2_seismic_count + l3_seismic_count + l4_seismic_count + l5_seismic_count - 1, get_risk_percent);
+    quickSort(cities, l1_seismic_count + l2_seismic_count + l3_seismic_count + l4_seismic_count + l5_seismic_count, cities_size - 1, get_risk_percent);
     printf("\n Segundo Sort: \n");
-    // for(int i = 0; i < cities_size; i++) {
-    //     printf("City: %s, Siesmic_Level: %d, Risk_Percent: %.2f\n", cities[i].city_name, cities[i].seismic_level, cities[i].risk_percent);
-    // }
+    for(int i = 0; i < cities_size; i++) {
+        printf("City: %s, Siesmic_Level: %d, Risk_Percent: %.2f\n", cities[i].city_name, cities[i].seismic_level, cities[i].risk_percent);
+    }
     // liberar memoria al final del programa
     free(cities);
     return 0;
-
 }
--- modules/processor.c
+++ modules/processor.c
@@ -3,107 +3,103 @@
 #include <string.h>
 #include "processor.h"
 
-
-
-void csv_processor(char *filename, City **cities, int *index) {
+void csv_processor(char *filename, City **cities, int *index)
+{
     // esta funcion parsea el csv en un array de structs tipo City, y devuelve el tamaño de este
 
-    char *column; //puntero para leer columnas
+    char *column;      // puntero para leer columnas
     char buffer[1024]; // almacenar linea
 
     // leer archivo
     FILE *file;
     file = fopen(filename, "r");
-    if (file == NULL) {
-      printf("Error, archivo nulo\n");
-      return ;
+    if (file == NULL)
+    {
+        printf("Error, archivo nulo\n");
+        return;
     }
-    
+
     // Contar cantidad de filas para asignar memoria dinamicamente al array cities
     int count = 0;
-    fgets(buffer, sizeof(buffer), file);  // Leer y saltar la cabecera
-    while (fgets(buffer, sizeof(buffer), file)) {
+    fgets(buffer, sizeof(buffer), file); // Leer y saltar la cabecera
+    while (fgets(buffer, sizeof(buffer), file))
+    {
         count++;
     }
     // printf("count %d\n", count);
 
-    // Asignar memoria para cities 
+    // Asignar memoria para cities
     *cities = malloc(count * sizeof(City));
-    if (*cities == NULL) {
+    if (*cities == NULL)
+    {
         printf("Error al asignar memoria\n");
         fclose(file);
-        return ;
+        return;
     }
 
     // Volver al inicio del archivo para leer los datos
-    fseek(file, 0, SEEK_SET);  
+    fseek(file, 0, SEEK_SET);
 
     // Leer la primera linea (nombres de columnas) para no almacenarlas en el struct
     fgets(buffer, sizeof(buffer), file);
 
-    while (fgets(buffer, sizeof(buffer), file)) {
+    while (fgets(buffer, sizeof(buffer), file))
+    {
 
         // Eliminar el salto de línea del final de la línea leída
         buffer[strcspn(buffer, "\n")] = '\0';
 
-
         // leer columna city_name
         column = strtok(buffer, ",");
         // printf("index %d\n",*index);
-        strcpy((*cities)[*index].city_name, column); 
+        strcpy((*cities)[*index].city_name, column);
         // printf("%s\n", (*cities)[*index].city_name);
 
-        //leer columna seismic_level y manejar caso null
+        // leer columna seismic_level y manejar caso null
         column = strtok(NULL, ",");
-        if (column != NULL){
+        if (column != NULL)
+        {
             // printf("columna seismic level %d\n",atoi(column));
             (*cities)[*index].seismic_level = atoi(column);
         }
 
         // leer columna risk_percent y manejar caso null
         column = strtok(NULL, ",");
-        if (column != NULL){
+        if (column != NULL)
+        {
             // printf("columna risk%f\n",atof(column));
-            if (strlen(column) == 0){
-                printf("Warning: risk_percent is empty, setting to NaN\n");
-                (*cities)[*index].risk_percent = 0.0f/0.0f;
-            }else {
-                (*cities)[*index].risk_percent = atof(column);
-            }
+            (*cities)[*index].risk_percent = atof(column);
         }
-        (*index)++; 
-
+        (*index)++;
     }
 
     fclose(file);
 
-
-   return ;
-
-    
+    return;
 }
 
-void csv_output(City *cities, int size, char *filename, int cities_to_priorize) {
+void csv_output(City *cities, int size, char *filename, int cities_to_priorize)
+{
     FILE *file = fopen(filename, "w");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("no se pudo escribir el archivo de salida");
         return;
     }
 
-    //encabezado
-    fprintf(file ,"city_name,seismic_level,risk_percent\n");
+    // encabezado
+    fprintf(file, "city_name,seismic_level,risk_percent\n");
 
-    //escribir datos del array de struct
-    for (int i = 0; i < cities_to_priorize; i++) {
+    // escribir datos del array de struct
+    for (int i = 0; i < cities_to_priorize; i++)
+    {
         fprintf(file, "%s,%d,%.2f\n",
                 cities[i].city_name,
                 cities[i].seismic_level,
-                cities[i].risk_percent
-        );
+                cities[i].risk_percent);
     }
 
     fclose(file);
     printf("CSV generado como %s con %d ciudades", filename, cities_to_priorize);
     return;
-
 }
